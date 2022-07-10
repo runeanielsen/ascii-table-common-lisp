76b2e7e790cffa4c1d28c3b7ff11fc48ff7beb42
Rune Nielsen <runenielsen@runbox.com>
2022-07-11 00:52:26 +0200

    move format char and format block into ascii-row

--- src/main.lisp
+++ src/main.lisp
@@ -1,21 +1,17 @@
 (defun ascii-header ()
   (format nil "~{~A~^ | ~}" (make-list 4 :initial-element "Dec  Hex  Oct  C") ))
 
-(defun format-char (c)
-  (if (or (> 32 c) (= c 127)) #\space (code-char c)))
-
-(defun format-block (n)
-  (format nil "~3d ~4x ~4o  ~c" n n n (format-char n)))
-
 (defun ascii-row (blocks)
-  (format nil "~{~A~^ | ~}" (map 'list #'format-block blocks)))
+  (labels ((format-char (c) (if (or (> 32 c) (= c 127)) #\space (code-char c)))
+           (format-block (n) (format nil "~3d ~4x ~4o  ~c" n n n (format-char n))))
+    (format nil "~{~A~^ | ~}" (map 'list #'format-block blocks))))
 
 (defun ascii-rows ()
   (let ((rows (loop for i from 0 to 31 collect (list i (+ i 32) (+ i 64) (+ i 96)))))
     (map 'list #'ascii-row rows)))
 
 (defun ascii-table ()
-  (format nil "~A~%~{~A~%~}" ( header) (ascii-rows)))
+  (format nil "~A~%~{~A~%~}" (ascii-header) (ascii-rows)))
 
 (defun main ()
   (format t "~A" (ascii-table)))
